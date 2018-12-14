0141abc3d9ff089fe5773a8a8f9ba6cadf5d0e69
Paweł Beder <pawel.beder@gmail.com>
2018-12-14 20:18:09 +0100

    Spring-rest example.

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String GREETING_TEMPLATE = "Hello %s";
+    private static final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(GREETING_TEMPLATE, name));
+    }
+}
